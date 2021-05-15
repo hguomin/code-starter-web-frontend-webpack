c570b07a97e2d6677b6fe066a08e9e4b9c36a281
Guomin Huang <hguomin@outlook.com>
2021-05-15 14:54:04 +0800

    add vscode debug configuration

--- webpack.development.js
+++ webpack.development.js
@@ -1,6 +1,7 @@
 const path = require('path');
 console.log("development environment...");
 module.exports = {
+    devtool: "source-map",
     devServer: {
         contentBase: path.resolve(__dirname, 'dist'),
         hot: true,
